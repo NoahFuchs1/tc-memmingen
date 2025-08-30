1b5a1dfd115f14a5bbb2371cd0d9223d31f01ec5
Noah <noah.fuchs@icloud.com>
2025-08-30 17:30:43 +0200

    bugfix termine und bildergalerie

---
+++ src/app/termine-und-bildergalerie/page.tsx
@@ -0,0 +1,12 @@
+import Footer from "@/src/components/layout/Footer";
+import { Header } from "@/src/components/layout/Header";
+
+export default function TermineUndBildergalerie() {
+  return (
+    <div className="min-h-[100vh] flex flex-col justify-between">
+      <Header></Header>
+      <div className="container"></div>
+      <Footer></Footer>
+    </div>
+  );
+}
